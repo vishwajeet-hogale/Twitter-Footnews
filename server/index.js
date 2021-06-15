bb463cddeee6ea38e79fb2d8c83f51779bd5831c
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-06-15 16:48:06 +0530

    Adding content

--- server/index.js
+++
@@ -1,6 +0,0 @@
-const needle = require('needle')
-const config = require('dotenv').config()
-const TOKEN = process.env.TWITTER_BEARER_TOKEN
-
-const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
-const 
---
+++ twitter/src/App.js
@@ -0,0 +1,12 @@
+import Card from '../src/components/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
